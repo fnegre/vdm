00a5dddff8a16ab67d8695e8ca87477f3a7b3942
Florian Negre <florian.negre@gmail.com>
2017-05-04 20:49:00 +0200

    Init application with Spring Initializr

---
+++ src/main/java/fr/fnegre/VdmApplication.java
@@ -0,0 +1,12 @@
+package fr.fnegre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VdmApplication.class, args);
+	}
+}
